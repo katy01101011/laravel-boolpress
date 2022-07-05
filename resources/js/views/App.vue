b9dc44c6a23e42356cf5f946e0323e7b2f821df0
katy01101011 <katy.91560@gmail.com>
2022-07-06 01:59:11 +0200

    add: Creato il frontoffice

---
+++ resources/js/views/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>
+        Ciao! Sono la pagina di frontoffice
+    </h1>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'App',
+}
+</script>
+
+<style>
+
+</style>
