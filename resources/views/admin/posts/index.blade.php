8cf17dd79035db4122bccf3e3af2239262a20adc
katy01101011 <katy.91560@gmail.com>
2022-07-07 01:09:08 +0200

    add: posts.index

---
+++ resources/views/admin/posts/index.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.dashboard')
+
+@section('content')
+    <h1>
+        Lista dei tuoi post
+    </h1>
+
+    <div class="row row-cols-4">
+        @foreach ($posts as $post)
+            {{-- Single post --}}
+            <div class="col">
+                <div class="card mb-4" style="width: 18rem;">
+                    {{-- <img src="..." class="card-img-top" alt="..."> --}}
+                    <div class="card-body">
+                        <h5 class="card-title">{{ $post->title }}</h5>
+                        <a href="#" class="btn btn-primary">Leggi post</a>
+                    </div>
+                </div>
+            </div>
+            {{-- Single post --}}
+        @endforeach
+    </div>
+@endsection
